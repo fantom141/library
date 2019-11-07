b35aa46c37c13438ca5b3b5ab9e31e8be79f1cf3
Alexander Balan <aleksandr.balan@hiendsys.com>
2019-11-08 01:59:52 +0300

    init

---
+++ projects/core/src/lib/core.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoreService {
+
+  constructor() { }
+
+  showMessage() {
+    console.log('messageeeeeeeeeeeee');
+
+  }
+}
